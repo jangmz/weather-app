1a523eeb25dbc24697c533874b57cfde7194a932
Jan <131482887+jangmz@users.noreply.github.com>
2024-02-13 17:37:35 +0530

    updated comments with needed data, changed some variables and test json file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ async function weatherFetch(location) {
     const weatherData = await weatherPromise.json();
     
     console.log(weatherData);
-    console.log(`Weather in ${location}, ${weatherData.location.country}: ${weatherData.current.condition.text} (${weatherData.current.temp_c}˚C)`);
+    console.log(`Weather in ${weatherData.location.name}, ${weatherData.location.country}: ${weatherData.current.condition.text} (${weatherData.current.temp_c}˚C)`);
 }
 
 weatherFetch("Ljubljana");
@@ -19,6 +19,16 @@ weatherFetch("Ljubljana");
     the information.
 [] Write the functions that process the JSON data you’re getting from the API and return 
     an object with only the data you require for your app.
+    Data needed: 
+        -Country, location
+        -timezone
+        -temperature in C and F
+        -feels like in C and F
+        -humidity
+        -last updated
+        -uv index
+        -condition text & icon
+        -wind speed in kmh and mph
 [] Set up a form that will let users input their location and will fetch the weather info 
     (still just console.log() it).
 [] Display the information on your webpage!
